6d17e962a35fc2a6ea8fe15a894a950eae8897c1
秉亮 <bl_indie@qq.com>
2014-07-30 22:57:22 +0800

    Scene updated&Animation input

---
+++ Scripts/BGMcontrol.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+@script RequireComponent(AudioSource)
+function Start () {
+audio.loop=true;
+}
+
+function Update () {
+
+}
+
+function OnGUI() {
+    if (GUILayout.Button("BGM pause"))
+    {if (!audio.isPlaying)
+    {audio.Play();}
+    else 
+    audio.Pause();
+    }
+}
